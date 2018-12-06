2da6398e929de2ea7763c8c3ecb21adc589b3700
Gifary Dhimas <gifarydhimas@gmail.com>
2018-12-06 09:58:48 +0700

    Add BGM to Main Menu

---
+++ Assets/Scripts/MainMenuAudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuAudioManager : MonoBehaviour
+{
+
+  public AudioClip BackgroundMusicClip;
+
+  public AudioSource BackgroundMusicSource;
+
+  void Start()
+  {
+    BackgroundMusicSource.clip = BackgroundMusicClip;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (!BackgroundMusicSource.isPlaying)
+      BackgroundMusicSource.Play();
+  }
+}
